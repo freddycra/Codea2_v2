03c26243e0432fd17513b3b4cd730f9172ce1234
CarlosMurillo2897 <42428269+CarlosMurillo2897@users.noreply.github.com>
2019-04-03 16:45:48 -0600

    Creating Activities.
    Main, Scores, Questions.

--- Codea2/app/src/main/java/com/example/codea2/MainActivity.java
+++ Codea2/app/src/main/java/com/example/codea2/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.codea2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -18,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
 
         getSupportActionBar().setTitle("Inicio");
+
+        Button MiButton = (Button) findViewById(R.id.jugar);
+        MiButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+             Intent intent = new Intent(getApplicationContext(), Preguntas.class);
+             startActivity(intent);
+            }
+        });
 
     } // Fin del Oncreate de la Actividad 01
 
@@ -34,12 +47,15 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         switch (id) {
-            case R.id.item1: Mensaje("Primero"); break;
-            case R.id.item2: Mensaje("Segundo"); break;
+            case R.id.item1:
+
+                break;
+            case R.id.item2:
+
+                break;
             default:  Mensaje("No clasificado"); break;
         }
         return super.onOptionsItemSelected(item);
     }
-
 
 } // [11:00:42 PM] Fin de la Clase Actividad 01
---
+++ Codea2/app/src/main/java/com/example/codea2/Puntajes.java
@@ -0,0 +1,24 @@
+package com.example.codea2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Puntajes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puntajes);
+
+        // A continuación mi código para OnCreate
+        Mensaje("Puntajes globales.");
+
+        getSupportActionBar().setTitle("Puntajes más altos");
+
+    } // Fin del Oncreate de la Actividad 03
+
+    public void Mensaje(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();};
+
+} // [4:42:01 PM] Fin de la Clase Actividad 03
