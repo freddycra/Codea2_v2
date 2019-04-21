fde752d76beac469bc53643474486e6614d152fc
kristyn88 <kristyn.tenorio@gmail.com>
2019-04-21 14:17:55 -0600

    Juego, agregar preguntas y login terminados

--- Codea2/app/src/main/java/com/example/codea2/Preguntas.java
+++ Codea2/app/src/main/java/com/example/codea2/Preguntas.java
@@ -1,6 +1,14 @@
 package com.example.codea2;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.media.AudioManager;
+import android.media.Image;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,9 +17,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,6 +31,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -28,6 +40,8 @@ public class Preguntas extends AppCompatActivity {
 
     Pregunta pregunta;
     int puntos = 0;
+    ProgressDialog pd;
+
     //TODO agregar un array con las preguntas que ya se han contestado
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +56,34 @@ public class Preguntas extends AppCompatActivity {
         OnclickDelButton(R.id.r3);
         OnclickDelButton(R.id.r4);
 
+        Button aux = (Button) findViewById(R.id.reproducirAudio);
+        aux.setVisibility(aux.INVISIBLE); // Se hace invisible el botón de reproducir
+
+        VideoView video = (VideoView) findViewById(R.id.reproducirVideo);
+        video.setVisibility(video.INVISIBLE); // Se hace invisible el botón de reproducir video
+
+        ImageView MiImageView = (ImageView) findViewById(R.id.volverRepreoducir);
+        MiImageView.setVisibility(MiImageView.INVISIBLE);
+
         leerPregunta();
 
     } // Fin del Oncreate de la Actividad 02
 
     public void leerPregunta(){
+        Button aux = (Button) findViewById(R.id.reproducirAudio);
+        aux.setVisibility(aux.INVISIBLE); // Se hace invisible el botón de reproducir
+
+        VideoView video = (VideoView) findViewById(R.id.reproducirVideo);
+        video.setVisibility(video.INVISIBLE); // Se hace invisible el botón de reproducir video
+
+        ImageView MiImageView = (ImageView) findViewById(R.id.volverRepreoducir);
+        MiImageView.setVisibility(MiImageView.INVISIBLE);
+
+        ImageView img = (ImageView) findViewById(R.id.imageView);
+        img.setVisibility(img.VISIBLE);
+        img.setImageResource(R.drawable.code);
+
+
         Random rand = new Random();
         int n = rand.nextInt(VariablesGlobales.getInstance().getContador())+1;
         Log.i("Valor aleatorio",String.valueOf(n));
@@ -76,6 +113,14 @@ public class Preguntas extends AppCompatActivity {
                     findViewById(R.id.r3).setVisibility(View.INVISIBLE);
                     findViewById(R.id.r4).setVisibility(View.INVISIBLE);
                 }
+
+
+                    try {
+                        cargarMultimedia(pregunta.getTipo(), pregunta.getUrl());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
             }
 
             @Override
@@ -83,6 +128,96 @@ public class Preguntas extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void cargarMultimedia(int tipo, final String urlPregunta) throws IOException {
+        if(tipo==1){
+            ImageView imgView = (ImageView) findViewById(R.id.imageView);
+            Glide.with(this)
+                    .load(urlPregunta)
+        .into(imgView);  // imageview object
+        }else if(tipo==2){
+            ImageView aux = (ImageView) findViewById(R.id.imageView);
+            aux.setVisibility(aux.INVISIBLE); // Se hace invisible el image view4
+
+            Button reproducir = (Button) findViewById(R.id.reproducirAudio);
+            reproducir.setVisibility(reproducir.VISIBLE); // Se hace visible el botón de reproducir audio
+
+
+            reproducir.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+
+                public void onClick(View arg0) {
+                    MediaPlayer mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    try {
+                        mediaPlayer.setDataSource(urlPregunta);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        mediaPlayer.prepare(); // might take long! (for buffering, etc)
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    mediaPlayer.start();
+
+                }
+
+            });
+
+        }else if(tipo==3){
+
+            ImageView aux = (ImageView) findViewById(R.id.imageView);
+            aux.setVisibility(aux.INVISIBLE); // Se hace invisible el image view4
+
+            VideoView video = (VideoView) findViewById(R.id.reproducirVideo);
+            video.setVisibility(video.VISIBLE); // Se hace invisible el botón de reproducir video
+
+
+
+            pd = new ProgressDialog(Preguntas.this);
+            pd.setMessage("Buffering video please wait...");
+            pd.show();
+
+            Uri uri = Uri.parse(urlPregunta);
+            video.setVideoURI(uri);
+            video.start();
+
+            video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    //close the progress dialog when buffering is done
+                    pd.dismiss();
+                }
+            });
+
+            ImageView MiImageView = (ImageView) findViewById(R.id.volverRepreoducir);
+            MiImageView.setVisibility(MiImageView.VISIBLE);
+
+            MiImageView.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+
+                public void onClick(View arg0) {
+                    VideoView video = (VideoView) findViewById(R.id.reproducirVideo);
+                    Uri uri = Uri.parse(urlPregunta);
+                    video.setVideoURI(uri);
+                    video.start();
+
+                    video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            //close the progress dialog when buffering is done
+                            pd.dismiss();
+                        }
+                    });
+
+                }
+
+            });
+        }
     }
 
 
@@ -148,9 +283,38 @@ public class Preguntas extends AppCompatActivity {
             System.out.println("Puntos: "+puntos);
             leerPregunta();
         }else{
-            finish();
+            DialogSiNO_01();
         }
     }
+
+    public void  compartirResultados(String mensaje){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, mensaje);
+        startActivity(Intent.createChooser(intent, "Share with"));
+    }
+
+
+    public void DialogSiNO_01(){
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("Desea compartir el puntaje obtenido?");
+        builder1.setCancelable(true);
+        builder1.setPositiveButton("Si",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        String mensaje = "Hola! Mira mi nuevo resultado en Codea2:  " + String.valueOf(VariablesGlobales.getInstance().getContador() + "pts!");
+                        compartirResultados(mensaje);
+                        finish();
+                    } });
+        builder1.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Mensaje("END GAME");
+                        finish();
+                    } });
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    };
 
     public void Mensaje(String msg){ Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show(); };
 
